10493aa8c92b1179a24749798bb90c02d17481d3
ForeignWars <81156279+ForeignWars@users.noreply.github.com>
2021-03-24 10:19:59 +0100

    Update principal.py

--- principal.py
+++ principal.py
@@ -5,7 +5,8 @@ import time
 from pygame.locals impor
 
 limitrove = {
-  '':{'':,},
-  
+  'thukten':{lildeilon,inno-6,aorizia},
+  'lildeilon':{thukten,inno,ordogas,amlivor},
+}
 
 #la région=toute les régions limitroves.
