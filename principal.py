901e1f59ac24787384f23d9bfd849ddce7f615b0
ForeignWars <81156279+ForeignWars@users.noreply.github.com>
2021-03-24 10:32:06 +0100

    Update principal.py

--- principal.py
+++ principal.py
@@ -6,7 +6,21 @@ from pygame.locals impor
 
 limitrove = {
   'thukten':{lildeilon,inno-6,aorizia},
-  'lildeilon':{thukten,inno,ordogas,amlivor},
+  'lildeilon':{thukten,inno-6,ordogas,amlivor},
+  'inno-6':{thukten,lildeilon,aorizia,amlivor,azaire},
+  'aorizia':{thukten,inno-6,azaire,}
+  'ordogas':{lildeilon,amlivor,baerdan,krar-zimak,}
+  'amlivor':{lildeilon,inno-6,ordogas,azaire,baerdan,frodrine}
+  'azaire':{inno-6,aorizia,amlivor,frodrine}
+  'baerdan':{ordogas,amlivor,krar-zimak,frodrine}
+  'krar-zimak':{ordogas,baerdan,frodrine,vaedran,andrinos}
+  'frodrine':{amlivor,azaire,baerdan,krar-zimak,vaedran}
+  '':{}
+  '':{}
+  '':{}
+  '':{}
+  '':{}
+  '':{}
 }
 
 #la région=toute les régions limitroves.
