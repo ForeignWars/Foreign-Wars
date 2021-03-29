61c0175f37db1ab070a1ac0cb6811e3574acab64
ForeignWars <81156279+ForeignWars@users.noreply.github.com>
2021-03-29 12:00:56 +0200

    Update faction.py

--- faction.py
+++ faction.py
@@ -2,5 +2,18 @@ import pygame
 from nation import Nation
 
 class Faction:
-  def __init__ (self, start, gold, resource, power, commander, units):
-    j
+  def __init__ (self, start, gold, resource, power, commander, units, army):
+    self.start=start
+    self.gold=gold
+    self.resource=resource
+    self.power=power
+    self.commander=commander
+    self.units=[]
+    self.army=army
+  def attack (self, nation):
+    if nation.health>=self.army:
+      nation.health-=self.army
+    else:
+      nation.health=0
+  def recruit (self,nation):
+    
