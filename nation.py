48b1bcb23ebf4632e9325b477fd6554bf431f9f3
ForeignWars <81156279+ForeignWars@users.noreply.github.com>
2021-03-31 11:27:14 +0200

    Update nation.py

--- nation.py
+++ nation.py
@@ -3,11 +3,14 @@ import time
 
 class Nation:
   pygame.init()
-  def __init__(self, color, health, graph):
+  def __init__(self, color, health, graph, x, y):
     self.color = color
     self.health = health
     self.graph = graph
     self.units=[]
+    self.x=x
+    self.y=y
   def faction(self, faction):
     faction.units.append(self.units)
-    
+  def conquered (self.faction):
+    fenetre.blit(emblem, (self.x, self.y))
