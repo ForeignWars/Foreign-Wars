06f93ed1509e8066be0d535989900b3c9812185b
ForeignWars <81156279+ForeignWars@users.noreply.github.com>
2021-04-02 09:20:53 +0200

    Update races.py

--- races.py
+++ races.py
@@ -3,6 +3,7 @@ from faction import Faction
 
 class Dwarf (Faction):
   def __init__(self, start, mithril, gyrocopter, dwarf_emblem):
+    super().__init__(gold, army)
     self.start=(start.x, start.y)
     self.mithril=mithril
     self.gyrocopter=gyrocopter
