1f0fbf296f7b5ef951809ca3175c90f68abddfb2
ForeignWars <81156279+ForeignWars@users.noreply.github.com>
2021-04-02 09:53:28 +0200

    Update races.py

--- races.py
+++ races.py
@@ -1,12 +1,19 @@
 import pygame
 from faction import Faction
+from nation import Nation
 
 class Dwarf (Faction):
   def __init__(self, start, mithril, gyrocopter, dwarf_emblem):
-    super().__init__(gold, army)
+    Faction.__init__(self, gold, army)
+    self.gold=5
+    self.army=0
     self.start=(start.x, start.y)
     self.mithril=mithril
     self.gyrocopter=gyrocopter
     self.dwarf_emblem=dwarf_emblem
+    self.nations=[start]
+  def conquered_nations():
+    for country in self.nations:
+      country.conquered
   def fortress():
     toto=toto
