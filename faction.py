e3dbea3c4127e0d09a38efe6f5478f6ae8d2b7a9
ForeignWars <81156279+ForeignWars@users.noreply.github.com>
2021-04-07 10:31:05 +0200

    Update faction.py

--- faction.py
+++ faction.py
@@ -8,7 +8,7 @@ class Faction:
     self.attack_units=[]
     self.army=army
     self.attack_army=self.army-1
-  def attack (self, nation):
+  def attack (self, Nation):
     self.attack_units.append(self.units)
     self.units.clear()
     if '1' in self.attack_units:
@@ -23,30 +23,31 @@ class Faction:
     elif '4' in self.attack_units:
       self.attack_units.remove('4')
       self.units.append('4')
-    if nation.health>=self.attack_army:
-      nation.health-=self.attack_army
+    if Nation.health>=self.attack_army:
+      Nation.health-=self.attack_army
     else:
-      leftover = self.army - nation.health
+      leftover = self.army - Nation.health
       while leftover>0:
-        if self.army - nation.health >= 4:
-          if '4' in nation.units:
-            nation.units.remove('4')
+        if self.army - Nation.health >= 4:
+          if '4' in Nation.units:
+            Nation.units.remove('4')
             leftover-=4
             self.attack_units.remove('4')
-          elif '3' in nation.units:
-            nation.units.remove('3')
+          elif '3' in Nation.units:
+            Nation.units.remove('3')
             leftover-=3
             self.attack_units.remove('3')
-          elif '2' in nation.units:
-            nation.units.remove('2')
+          elif '2' in Nation.units:
+            Nation.units.remove('2')
             leftover-=2
             self.attack_units.remove('2')
-          elif '1' in nation.units:
-            nation.units.remove('1')
+          elif '1' in Hation.units:
+            Nation.units.remove('1')
             leftover-=1
             self.attack_units.remove('1')
           else:
-            self.nations.append(nation)
-            nation.units.append(self.attack_units)
-      nation.health=0
-  def recruit (self,nation):
+            self.nations.append(Nation)
+            Nation.units.append(self.attack_units)
+      Nation.health=0
+  def recruit (self,Nation):
+     toto=toto
