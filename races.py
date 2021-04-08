732c9bc023607e153f089d506d3ffd7b3e88d0bd
ForeignWars <81156279+ForeignWars@users.noreply.github.com>
2021-04-08 10:43:35 +0200

    Update races.py

--- races.py
+++ races.py
@@ -32,7 +32,25 @@ class Orc (Faction):
   def conquered_nations():
     for country in self.nations:
       country.conquered
-  def waaagh (Nation):
+  def waaagh ():
     if rage>=50:
       self.army=self.army*2
-      self.units
+      self.units+=self.units
+      
+class Demon (Faction):
+  def __init__(self, start, souls, mephistopheles, demon_emblem):
+    Faction.__init__(self, gold, army)
+    self.gold=5
+    self.army=0
+    self.start=(start.x, start.y)
+    self.souls=souls
+    self.mephistopheles=mephistopheles
+    self.demon_emblem=demon_emblem
+    self.nations=[start]
+  def conquered_nations():
+    for country in self.nations:
+      country.conquered
+  def corruption(Nation):
+    if souls>=80:
+      self.nations.append(Nation)
+      Nation.conquered
