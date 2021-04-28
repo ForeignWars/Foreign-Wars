75b6a0025b6263f1a3835f44aad219c5ca266b3d
ForeignWars <81156279+ForeignWars@users.noreply.github.com>
2021-04-28 10:46:21 +0200

    Update races.py

--- races.py
+++ races.py
@@ -1,66 +1,66 @@
 import pygame
-from faction import Faction
-from nation import Nation
+from faction import *
+from nation import *
 
 class Dwarf (Faction):
-  def __init__(self, start, mithril, gyrocopter, dwarf_emblem):
-    Faction.__init__(self, gold, army)
-    self.start=(start.x, start.y)
-    self.mithril=mithril
-    self.gyrocopter=gyrocopter
-    self.dwarf_emblem=dwarf_emblem
-    self.nations=[start]
-  def conquered_nations():
-    for country in self.nations:
-      country.conquered
-  def fortress(Nation):
-    if mithril>=30:
-      Nation.health+=10
+    def __init__(self, start, mithril, gyrocopter, dwarf_emblem):
+        super(Faction).__init__()
+        self.start=(start.x, start.y)
+        self.mithril=mithril
+        self.gyrocopter=gyrocopter
+        self.dwarf_emblem=dwarf_emblem
+        self.nations=[start]
+    def conquered_nations():
+        for country in self.nations:
+            country.conquered
+    def fortress(Nation):
+        if mithril>=30:
+            Nation.health+=10
 
 class Orc (Faction):
-  def __init__(self, start, rage, berzerker, orc_emblem):
-    Faction.__init__(self, gold, army)
-    self.start=(start.x, start.y)
-    self.rage=rage
-    self.berzerker=berzerker
-    self.orc_emblem=orc_emblem
-    self.nations=[start]
-  def conquered_nations():
-    for country in self.nations:
-      country.conquered
-  def waaagh ():
-    if rage>=50:
-      self.army=self.army*2
-      self.units+=self.units
-      
+    def __init__(self, start, rage, berzerker, orc_emblem):
+        super(Faction).__init__()
+        self.start=(start.x, start.y)
+        self.rage=rage
+        self.berzerker=berzerker
+        self.orc_emblem=orc_emblem
+        self.nations=[start]
+    def conquered_nations():
+        for country in self.nations:
+            country.conquered
+    def waaagh ():
+        if rage>=50:
+            self.army=self.army*2
+            self.units+=self.units
+
 class Demon (Faction):
-  def __init__(self, start, souls, mephistopheles, demon_emblem):
-    Faction.__init__(self, gold, army)
-    self.start=(start.x, start.y)
-    self.souls=souls
-    self.mephistopheles=mephistopheles
-    self.demon_emblem=demon_emblem
-    self.nations=[start]
-  def conquered_nations():
-    for country in self.nations:
-      country.conquered
-  def corruption(Nation):
-    if souls>=80:
-      self.nations.append(Nation)
-      Nation.conquered
-      Nation.units.clear()
-     
+    def __init__(self, start, souls, mephistopheles, demon_emblem):
+        super(Faction).__init__()
+        self.start=(start.x, start.y)
+        self.souls=souls
+        self.mephistopheles=mephistopheles
+        self.demon_emblem=demon_emblem
+        self.nations=[start]
+    def conquered_nations():
+        for country in self.nations:
+            country.conquered
+    def corruption(Nation):
+        if souls>=80:
+            self.nations.append(Nation)
+            Nation.conquered
+            Nation.units.clear()
+
 class Centaure (Faction):
-  def __init__(self, start, totems, centaurion, centaur_emblem):
-    Faction.__init__(self, gold, army)
-    self.start=(start.x, start.y)
-    self.totems=totems
-    self.centaurion=centaurion
-    self.centaur_emblem=centaur_emblem
-    self.nations=[start]
-  def conquered_nations():
-    for country in self.nations:
-      country.conquered
-  def volley(Nation):
-    if totems>=16:
-      Nation.health-=5
+    def __init__(self, start, totems, centaurion, centaur_emblem):
+        super(Faction).__init__()
+        self.start=(start.x, start.y)
+        self.totems=totems
+        self.centaurion=centaurion
+        self.centaur_emblem=centaur_emblem
+        self.nations=[start]
+    def conquered_nations():
+        for country in self.nations:
+            country.conquered
+    def volley(Nation):
+        if totems>=16:
+            Nation.health-=5
